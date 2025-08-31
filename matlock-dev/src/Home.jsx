31859de2b956ff6a2f636077e50df5388473bc3c
oMatlocko <113477748+oMatlocko@users.noreply.github.com>
2025-08-31 13:03:39 -0600

    added icons for project tools

--- matlock-dev/src/Home.jsx
+++ matlock-dev/src/Home.jsx
@@ -5,32 +5,53 @@ function Home() {
         <>
             <div className="card-container">
                 <Link to="/about" className="card vertical-long">
-                    <time datetime="2025-08-22">
-                    Aug 22, 2025
-                    </time>
+                    <time>Aug 22, 2025</time>
                     <h2>About Me</h2>
                     <p>Welcome to my website! My name is Garrett. I'm a recent Computing Science graduate from the University of Alberta.</p>
                     <p>matlock.dev is meant to function as a portfolio where I can showcase some of my project work. I'm also using it as a way to enhance my front-end web development skills.</p>
                     <p>If you'd like to learn more about me and what I do, click on 'About' in the navigation bar at the top-right of the page, or just click on this card!</p>
-
+                    <div className="tool-container">
+                    </div>
                 </Link>
                 <div className="card">
-                    <img src="gazprea-icon.png" alt="gaz-logo" className="card-icon"/>
-                    <h2>Gazprea Compiler</h2>
-                    <p>A custom compiler for the Gazprea programming language developed with C++, LLVM, MLIR, and ANTLR4!</p>
-
+                    <h2>
+                        Gazprea Compiler
+                        {/* <img src="gazprea-icon.png" className="card-icon"/> */}
+                    </h2>
+                    <p>A custom compiler for the Gazprea programming language</p>
+                    <div className="tool-container">
+                        <img src="cpp-logo.png" className="tool"/>
+                        <img src="llvm-dragon.png" className="tool"/>
+                        <img src="mlir-logo.png" className="tool"/>
+                        <img src="antlr-logo.png" className="tool"/>
+                    </div>
                 </div>
                 <div className="card">
                     <h2>QRiffic</h2>
+                    <p>A location-based AR scavenger hunt game for Android</p>
+                    <div className="tool-container">
+                        <img src="Android-Studio-icon.svg" className="tool"/>
+                        <img src="Java_logo.png" className="tool"/>
+
+                    </div>
                 </div>
                 <div className="card">
                     <h2>matlock.dev</h2>
                     <p>Take a look into matlock.dev's development</p>
+                    <div className="tool-container">
+                        <img src="React-icon.svg" className="tool"/>
+                        <img src="CSS-logo.png" className="tool"/>
+                        <img src="Vitejs-logo.svg" className="tool"/>
+                    </div>
                 </div>
                 <div className="card">
                     <h2>PROJECT: Pantry</h2>
-                    <p>Full-Stack home inventory management app</p>
-                    <p>WIP</p>
+                    <p>Home inventory management app. WIP</p>
+                    <div className="tool-container">
+                        <img src="Tauri.png" className="tool"/>
+                        <img src="Rust-logo.svg" className="tool"/>
+                        <img src="Sqlite-square-icon.png" className="tool"/>
+                    </div>
                 </div>
             </div>
         </>
